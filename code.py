d7174ddebf589efa77eca7ba3b7a89ee80239be8
Danny <dannylee94049@gmail.com>
2020-12-12 21:17:30 +0800

    update rocchio
    voc just query or whole words

--- code.py
+++ code.py
@@ -37,7 +37,7 @@ class dotdict(dict):
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
-args = dotdict(A=1, B=0.8, C=0.1, step=5, scratch=0)
+args = dotdict(A=1, B=0.8, C=0.1, step=5, scratch=1, save=0)
 
 # bm_B = args.bm_B
 # K1 = args.K1
@@ -50,17 +50,18 @@ C = args.C
 
 #%%
 timestamp()
-doc_path = "data/docs/"
-query_path = "data/queries/"
+root_path = "../HW1 Vector Space Model/"
+doc_path = root_path+"data/docs/"
+query_path = root_path+"data/queries/"
 
 d_list = []
-with open('data/doc_list.txt', 'r') as d_list_file:
+with open(root_path+'data/doc_list.txt', 'r') as d_list_file:
     for line in d_list_file:
         line = line.replace("\n", "")
         d_list += [line]
 
 q_list = []
-with open('data/query_list.txt', 'r') as q_list_file:
+with open(root_path+'data/query_list.txt', 'r') as q_list_file:
     for line in q_list_file:
         line = line.replace("\n", "")
         q_list += [line]
@@ -74,10 +75,26 @@ for txt in tqdm(file_iter("q")):
     list_q_tf += [Counter(txt.split())]
     query_list += [txt]
 
-for txt in tqdm(file_iter("d")):
-    list_d_tf += [Counter(txt.split())]
-    doc_list += [txt]
-    doc_len += [len(txt.split())]
+if args.scratch:
+    for txt in tqdm(file_iter("d")):
+        list_d_tf += [Counter(txt.split())]
+        doc_list += [txt]
+        doc_len += [len(txt.split())]
+    if args.save:
+        with open("model/list_d_tf.pkl", "wb") as fp:
+            pickle.dump(list_d_tf, fp)
+        with open("model/doc_list.pkl", "wb") as fp:
+            pickle.dump(doc_list, fp)
+        with open("model/doc_len.pkl", "wb") as fp:
+            pickle.dump(doc_len, fp)
+else:
+    with open("model/list_d_tf.pkl", "rb") as fp:
+        list_d_tf = pickle.load(fp)
+    with open("model/doc_list.pkl", "rb") as fp:
+        doc_list = pickle.load(fp)
+    with open("model/doc_len.pkl", "rb") as fp:
+        doc_len = pickle.load(fp)
+
 doc_avg_len = np.array(doc_len).mean()
 total_len = sum(doc_len)
 
@@ -100,9 +117,6 @@ def counter2array(voc, li, path, list_of_counter):
     col = []
     data = []
     for i, w_i in tqdm(enumerate(voc)):
-        row += [i]
-        col += [0]
-        data += [0]
         for j in range(len(li)):
             if list_of_counter[j][w_i] != 0:
                 row += [i]
@@ -111,12 +125,14 @@ def counter2array(voc, li, path, list_of_counter):
     row = np.array(row)
     col = np.array(col)
     data = np.array(data)
-    tf_array = coo_matrix((data, (row, col)))
-    sparse.save_npz(path, tf_array)
+    tf_array = coo_matrix((data, (row, col)), shape=(len(voc), len(li)))
+    if args.save:
+        sparse.save_npz(path, tf_array)
     del row,col,data
     return tf_array
 
 #%%
+# tf array
 if args.scratch:
     tf_array = counter2array(voc, d_list, 'model/tf_array.npz', list_d_tf)
 else:
@@ -125,20 +141,19 @@ else:
 #%%
 # df
 if args.scratch:
-    with open("model/voc.pkl", "rb") as fp:
-        voc = pickle.load(fp)
-    doc_len = []
+    # with open("model/voc.pkl", "rb") as fp:
+    #     voc = pickle.load(fp)
     bm_df = dict.fromkeys(voc, 0)
     BG = [0] * len(voc)
     for j, doc in tqdm(enumerate(doc_list)):
-        doc_len += [len(doc)]
         for i, w in enumerate(voc):
             bm_df[w] += 1 if list_d_tf[j][w] > 0 else 0
             BG[i] += list_d_tf[j][w]
     BG = np.array(BG)
-    with open("model/bm_df.pkl", "wb") as fp:
-        pickle.dump(bm_df, fp)
-    np.save("model/BG.npy", BG)
+    if args.save:
+        with open("model/bm_df.pkl", "wb") as fp:
+            pickle.dump(bm_df, fp)
+        np.save("model/BG.npy", BG)
 else:
     with open("model/bm_df.pkl", "rb") as fp:
         bm_df = pickle.load(fp)
@@ -151,7 +166,7 @@ if args.scratch:
     small_tf_array = []
     small_voc = []
     small_BG = []
-    for arg in tqdm(BG.argsort()[-10000: ]):
+    for arg in tqdm(BG.argsort()[-10000: ]): # 15k~5k most fequent word [-15000: -5000]
         small_tf_array += [tf_array[arg]]
         small_voc += [voc[arg]]
         small_BG += [BG[arg]]
@@ -171,19 +186,48 @@ else:
     small_BG = np.load("model/small_BG.npy")
 
 #%%
+# query voc
+small_voc = q_voc
+tf_array = tf_array.tocsr()
+small_tf_array = []
+small_BG = []
+for v in small_voc: # query word
+    arg = voc.index(v)
+    small_tf_array += [tf_array[arg]]
+    small_BG += [BG[arg]]
+small_tf_array = sparse.vstack([row for row in small_tf_array])
+small_BG = np.array(small_BG)
+if args.save:
+    sparse.save_npz("model/query/small_tf_array.npz", small_tf_array)
+    np.save("model/query/small_BG.npy", small_BG)
+q_tf_array = counter2array(small_voc, q_list, 'model/query/q_tf_array.npz', list_q_tf)
+
+#%%
 #idf
-idf = np.log(1+(len(d_list)+1) / (small_tf_array.indptr[1:] - small_tf_array.indptr[:-1])+1)
+tf_array = tf_array.tocsr()
+n_i = tf_array.indptr[1:] - tf_array.indptr[:-1]+1
+idf = np.log(1 + len(d_list) / n_i)#ndarray
+# n_i = small_tf_array.indptr[1:] - small_tf_array.indptr[:-1]+1
+# idf = np.log(1 + len(d_list) / n_i)#ndarray
 
 #%%
 # tfidf
-q_tf_array = q_tf_array.transpose()
-small_tf_array = small_tf_array.transpose()
+# q_tf_array = counter2array(voc, q_list, 'model/q_tf_array.npz', list_q_tf)
+# q_tf_array = q_tf_array.transpose()
+# tf_array = tf_array.transpose()
+# np_q_tfidf = q_tf_array.multiply(idf).tocsr()
+# np_d_tfidf = tf_array.multiply(idf).tocsr()
 
+
+# small
+q_tf_array = q_tf_array.transpose()
+# small_tf_array = small_tf_array.transpose()
+# tf_array = tf_array.transpose()
 
 np_q_tfidf = (1 + np.ma.log2(q_tf_array.toarray())) * idf
-np_d_tfidf = (1 + np.ma.log2(small_tf_array.toarray())) * idf
-# small_tf_array.data = np.log2(small_tf_array.data)
-# np_d_tfidf = (1+ small_tf_array.toarray()) * idf
+np_d_tfidf = (1 + np.ma.log2(tf_array.toarray())) * idf
+# # small_tf_array.data = np.log2(small_tf_array.data)
+# # np_d_tfidf = (1+ small_tf_array.toarray()) * idf
 
 np_q_tfidf = np_q_tfidf.filled(0)
 np_d_tfidf = np_d_tfidf.filled(0)
@@ -210,7 +254,7 @@ sim_array = bm_sim_array
 #=================== BM25 ===================#
 
 #%%
-args = dotdict(A=1, B=0.2, C=0.15, step=10, r_doc=5, nr_doc=1, scratch=0)
+args = dotdict(A=1, B=0.75, C=0.15, step=10, r_doc=5, nr_doc=1, scratch=0)
 A = args.A
 B = args.B
 C = args.C
@@ -219,8 +263,8 @@ nr_doc = args.nr_doc
 
 #%%
 from sklearn.metrics.pairwise import cosine_similarity
-np_q_tfidf = (1 + np.ma.log2(q_tf_array.toarray())) * idf
-np_q_tfidf = np_q_tfidf.filled(0)
+# np_q_tfidf = (1 + np.ma.log2(q_tf_array.toarray())) * idf
+# np_q_tfidf = np_q_tfidf.filled(0)
 sim_array = cosine_similarity(np_q_tfidf, np_d_tfidf)
 sim_array = np.array(sim_array)
 #========== VSM ==========#
@@ -230,14 +274,14 @@ from sklearn.metrics.pairwise import cosine_similarity
 for step in range(args.step):
     new_q_tfidf = np.zeros(len(small_voc))
     for q in tqdm(range(len(q_list))):
-        relate_doc_vac = []
+        relate_doc_vec = []
         relate_arg = np.argsort(sim_array[q])
-        relate_doc_vac = np.vstack([np_d_tfidf[_, :] for _ in relate_arg[-r_doc:]])
-        relate_doc_vac = relate_doc_vac.mean(0)
-        non_relate_doc_vac = np.vstack([np_d_tfidf[_, :] for _ in relate_arg[:nr_doc]])
-        non_relate_doc_vac = non_relate_doc_vac.mean(0)
-        new_q_vac = A*np_q_tfidf[q] + B*relate_doc_vac - C*non_relate_doc_vac
-        new_q_tfidf = np.vstack((new_q_tfidf, new_q_vac))
+        relate_doc_vec = np.vstack([np_d_tfidf[_, :] for _ in relate_arg[-r_doc:]])
+        relate_doc_vec = relate_doc_vec.mean(0)
+        non_relate_doc_vec = np.vstack([np_d_tfidf[_, :] for _ in relate_arg[:nr_doc]])
+        non_relate_doc_vec = non_relate_doc_vec.mean(0)
+        new_q_vec = A*np_q_tfidf[q] + B*relate_doc_vec - C*non_relate_doc_vec
+        new_q_tfidf = np.vstack((new_q_tfidf, new_q_vec))
     new_q_tfidf = new_q_tfidf[1:]
     sim_array = cosine_similarity(new_q_tfidf, np_d_tfidf)
     sim_array = np.array(sim_array)
